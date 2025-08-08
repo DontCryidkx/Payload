e57d3bb96efa6c141bed8a1e47f6668fc75d748c
DontCry000x <kdflss165@gmail.com>
2025-08-08 23:38:38 +0200

    updated to 1.52

--- source/kern.c
+++ source/kern.c
@@ -15,7 +15,7 @@ Inline uint8_t * getKernelBase()
     //*(unsigned char *) (getKernelBase + 0x1BD800D) = 0x80;
 }
 
-int kernelPayload(struct thread *td, void* uap) {
+/*int kernelPayload(struct thread *td, void* uap) {
 	uint8_t* ptrKernel = getKernelBase();
 	struct ucred* cred = td->td_proc->p_ucred;
 	struct filedesc* fd = td->td_proc->p_fd;
@@ -79,6 +79,77 @@ int kernelPayload(struct thread *td, void* uap) {
     // patch kmem_alloc
     *(uint8_t *)(ptrKernel + 0x37BF3C) = VM_PROT_ALL;
     *(uint8_t *)(ptrKernel + 0x37BF44) = VM_PROT_ALL;
+
+	// Enable write protection
+	writeCr0(cr0);
+
+	return 0;
+}*/
+
+int kernelPayload(struct thread *td, void* uap) {
+	uint8_t* ptrKernel = getKernelBase();
+	struct ucred* cred = td->td_proc->p_ucred;
+	struct filedesc* fd = td->td_proc->p_fd;
+
+	// Escalate privileges
+	cred->cr_uid = 0;
+	cred->cr_ruid = 0;
+	cred->cr_rgid = 0;
+	cred->cr_groups[0] = 0;
+
+	// Escape sandbox
+	void** prison0 = (void**)&ptrKernel[KERN_PRISON_0];
+	void** rootvnode = (void**)&ptrKernel[KERN_ROOTVNODE];
+	cred->cr_prison = *prison0;
+	fd->fd_rdir = fd->fd_jdir = *rootvnode;
+
+	void *td_ucred = *(void **)(((char *)td) + 304); // p_ucred == td_ucred
+
+	// sceSblACMgrIsSystemUcred
+	uint64_t *sonyCred = (uint64_t *)(((char *)td_ucred) + 96);
+	*sonyCred = 0xffffffffffffffff;
+
+	// sceSblACMgrGetDeviceAccessType
+	uint64_t *sceProcType = (uint64_t *)(((char *)td_ucred) + 88);
+	*sceProcType = 0x3801000000000013; // Max access
+
+	// sceSblACMgrHasSceProcessCapability
+	uint64_t *sceProcCap = (uint64_t *)(((char *)td_ucred) + 104);
+	*sceProcCap = 0xffffffffffffffff; // Sce Process
+
+	// Disable write protection
+	uint64_t cr0 = readCr0();
+	writeCr0(cr0 & ~X86_CR0_WP);
+
+    //patch memcpy first 11.00
+    *(uint8_t *)(ptrKernel + 0x2DDDFD) = 0xEB;//11
+
+    // patch sceSblACMgrIsAllowedSystemLevelDebugging
+    memcpy((void *)(ptrKernel + 0x3D0DE0), "\x48\xC7\xC0\x01\x00\x00\x00\xC3", 8);
+
+    // patch sceSblACMgrHasMmapSelfCapability
+    memcpy((void *)(ptrKernel + 0x3D0E50), "\x48\xC7\xC0\x01\x00\x00\x00\xC3", 8);
+
+    // patch sceSblACMgrIsAllowedToMmapSelf
+    memcpy((void *)(ptrKernel + 0x3D0E70), "\x48\xC7\xC0\x01\x00\x00\x00\xC3", 8);
+
+    // disable sysdump_perform_dump_on_fatal_trap
+    // will continue execution and give more information on crash, such as rip
+    *(uint8_t *)(ptrKernel + 0x76D210) = 0xC3;
+
+    // self patches
+    memcpy((void *)(ptrKernel + 0x157F91), "\x31\xC0\x90\x90\x90", 5);
+
+	// patch ptrace, thanks 2much4u
+	*(uint8_t*)(ptrKernel + 0x384285) = 0xEB;
+	memcpy((void*)(ptrKernel + 0x384771), "\xE9\xD0\x00\x00\x00", 5);
+
+    // Disable process aslr
+    *(uint16_t*)&ptrKernel[KERN_PROCESS_ASLR] = 0x9090;//9
+
+    // patch kmem_alloc
+    *(uint8_t *)(ptrKernel + 0x245EDC) = VM_PROT_ALL;
+    *(uint8_t *)(ptrKernel + 0x245EE4) = VM_PROT_ALL;
 
 	// Enable write protection
 	writeCr0(cr0);
